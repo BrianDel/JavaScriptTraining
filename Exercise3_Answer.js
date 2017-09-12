dc8bf1879e27690f640a680842cd975b4ebc7906
BrianDel <brian.delahunty@gmail.com>
2017-09-12 09:39:22 +0100

    Upload correct version of E3 answer

--- Exercise3_Answer.js
+++ Exercise3_Answer.js
@@ -1,42 +1,47 @@
 function TodoList(name) {
-  this.name = name;
-  this.items = [];
+ this.name = name;
+ this.items = [];
+ this.getName = function(){
+   return name;
+ };
 
   // Create a monitor that periodically checks for overdue items.
   TodoList.prototype.overDueMonitor = function(todoList) {
     //console.log("Checking for overdue items in your " + todoList.name + " todo list...")
     items = todoList.items;
-    items.forEach(function(item) {
+    items.forEach(function(item){
       item.checkIfOverdue();
     });
   };
-  setInterval(this.overDueMonitor, 4000, this);
+  let intervalID = setInterval(this.overDueMonitor, 4000, this);
+
+  TodoList.prototype.getTodoList = function() {
+    return this.items;
+  };
 
   TodoList.prototype.printList = function() {
-    console.log(`\n ${this.name} (${this.items.length}) \n------------`);
-    this.items.forEach(function(item) {
-      var {description,priority,overdue} = item;
-      let overdueLabel = overdue ? "(Overdue)" : "";
-      console.log(`[${priority}] ${description} ${overdueLabel}`);
-    }, this);
+    console.log(`\n ${name} (${this.items.length}) \n------------`);
+    this.items.forEach(({description, priority, overdue}) =>
+      console.log(`[${priority}] .... ${description} ${overdue ? "(Overdue)" : "" }`)
+    );
   };
 
   TodoList.prototype.addItem = function(item) {
 
-    if (item !== null && item.description !== "") {
+    if (item!== null && item.getDescription()!==""){
       this.items.push(item);
-      console.log("Added item '" + item.description + "' to my '" + name + "' todo list");
+      console.log(`Added item '${item.getDescription()}' to my '${name}' todo list`);
     } else {
-      console.log("Failed to add an item to the " + name + " list as it is missing a description");
+      console.log(`Failed to add an item to the ${name} list as it is missing a description`);
     }
   };
 
   TodoList.prototype.removeItem = function(removeItem) {
-    if (removeItem.description !== undefined) {
+     if(removeItem.getDescription()!==undefined) {
 
-      this.items.splice(this.items.indexOf(removeItem), 1);
-    }
-  };
+       this.items.splice(this.items.indexOf(removeItem),1);
+     }
+   };
 
 }
 
@@ -62,14 +67,14 @@ function LowPriorityItem(itemDescription, dueIn) {
 // description(mandatory)
 // itemPriority (High, Medium, Low)
 // dueIn number of seconds from now until item is due
-function Item(itemDescription, itemPriority, dueIn = 0) {
+function Item(itemDescription, itemPriority, dueIn=0){
 
   // Create instance variables using this
   this.description = "";
-  var priorityTypes = ["High", "Medium", "Low"];
+  this.priorityTypes = ["High","Medium","Low"];
   this.priority = "Medium";
   this.overdue = false;
-  var dueDate = null;
+  this.dueDate = null;
   if (dueIn > 0) {
     this.dueDate = (Date.parse(new Date()) + (dueIn * 1000));
   }
@@ -80,49 +85,55 @@ function Item(itemDescription, itemPriority, dueIn = 0) {
     this.description = itemDescription;
   }
 
-  if (itemPriority !== undefined) {
+  if(itemPriority !== undefined) {
     // Ensure the priority is a valid selection
-    priorityTypes.forEach(function(type) {
-      if (itemPriority == type) {
-        this.priority = itemPriority;
+    var array = this.priorityTypes.filter((item) => item == itemPriority);
+    this.priority = this.priorityTypes.filter((item) => item == itemPriority)[0]!=null ? array[0] : "Medium";
 
-      }
-    }, this);
   }
 
+  // Share methods using the prototype reference.
+  Item.prototype.getDescription = function(){
+    return this.description;
+  };
+  Item.prototype.updateDescription = function(description){
+    this.description = description;
+  };
+  Item.prototype.getPriority = function(){
+      return this.priority;
+  };
   Item.prototype.checkIfOverdue = function() {
-    if (this.overdue) {
-      return true;
-    }
-    if (this.dueDate == null) {
-      return false;
-    }
-    let now = Date.parse(new Date());
-    this.overdue = this.dueDate - now < 0;
-    return this.overdue;
+      if(this.overdue) {
+        return true;
+      }
+      if(this.dueDate == null) {
+        return false;
+      }
+      let now = Date.parse(new Date());
+      this.overdue = this.dueDate - now < 0;
+      return this.overdue;
   };
 }
 
-
-var todoList = new TodoList("Housework");
-todoList.name;
+let todoList = new TodoList("Housework");
+todoList.getName;
 todoList.printList();
-var item1 = new HighPriorityItem("Take out the bins", 20);
+let item1 = new HighPriorityItem("Take out the bins",20);
 todoList.addItem(item1);
-var item2 = new MediumPriorityItem("Cut the grass");
+let item2 = new MediumPriorityItem("Cut the grass", 40);
 todoList.addItem(item2);
 todoList.printList();
-var item3 = new Item(null, "Low");
+let item3 = new Item(null,"Low");
 todoList.addItem(item3);
-var item4 = new Item("Do the shop", "Low");
+let item4 = new Item("Do the shop","Low");
 todoList.addItem(item4);
 todoList.printList();
 todoList.removeItem(item2);
 todoList.printList();
 
-var todoList2 = new TodoList("Office");
-todoList2.name;
+let todoList2 = new TodoList("Office");
+todoList2.getName;
 todoList2.printList();
-var item5 = new LowPriorityItem("Fill out hours sheet", 2);
+let item5 = new LowPriorityItem("Fill out hours sheet", 2);
 todoList2.addItem(item5);
 todoList2.printList();
